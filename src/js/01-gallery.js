8aa5ab7a745d7f1511dddafb1964d5413de77499
VitaliyLev <111646762+VitaliyLev@users.noreply.github.com>
2022-10-31 22:02:23 +0200

    add

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,71 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
-// Change code below this line
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 
-console.log(galleryItems);
+import { galleryItems } from './gallery-items';
+
+const galleryRef = document.querySelector('.gallery');
+
+const createCalleryItemMurkup = galleryItems
+  .map(
+    item =>
+      `<a class="gallery__item" href="${item.original}">
+            <img class="gallery__image" src="${item.preview}" alt="${item.description}" />
+        </a>`
+  )
+  .join('');
+
+galleryRef.insertAdjacentHTML('afterbegin', createCalleryItemMurkup);
+
+galleryRef.addEventListener('click', handleClick);
+
+function handleClick(event) {
+  event.preventDefault();
+  const handClick = event.target.nodeName;
+  if (handClick !== 'IMG') {
+    return;
+  }
+}
+
+const galleryAllImages = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+  animationSpeed: 300,
+  fadeSpeed: 350,
+});
+
+
+
+// const timeKey = "videoplayer-current-time";
+
+// const player = new Player('handstick', {
+//     id: 19231868,
+//     width: 640
+// });
+
+// // player.on('play', function() {
+// //     console.log('played the video!');
+// // });
+
+// player.on('currentTime', function(data) {
+//     timeKey = data;
+// });
+
+
+// // var callback = function() {};
+
+// // player.off('eventName', callback);
+
+
+// player.setCurrentTime(30.456).then(function(seconds) {
+//     // seconds = the actual time that the player seeked to
+// }).catch(function(error) {
+//     switch (error.name) {
+//         case 'RangeError':
+//             // the time was less than 0 or greater than the video’s duration
+//             break;
+
+//         default:
+//             // some other error occurred
+//             break;
+//     }
+// });
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,20 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+
+
+// const playerVimeoContent = new Vimeo.Player(iFrameEl);
+
+    // playerVimeoContent.on('play', function() {
+    //     console.log('played the video!');
+    // });
+
+
+const player = new Player('vimeo-player', {
+    id: 19231868,
+    width: 640
+});
+
+player.on('play', function() {
+    console.log('played the video!');
+});
 
