70d236b318fe298a6612c0eebe36fa20578e1a99
VitaliyLev <111646762+VitaliyLev@users.noreply.github.com>
2022-11-01 19:14:29 +0200

    add 3task

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,36 @@
+import throttle from 'lodash.throttle';
 
+const formEl = document.querySelector('.feedback-form');
+const keyLocaleStorage = 'feedback-form-state';
+
+textInAllFormInput();
+
+const formData = {};
+
+formEl.addEventListener('submit', onFormSubmit);
+formEl.addEventListener('input', throttle(onFormAllInput, 500));
+
+function onFormAllInput(e) {
+  formData[e.target.name] = e.target.value;
+
+  inputStorageArrayJson = JSON.stringify(formData);
+
+  localStorage.setItem(keyLocaleStorage, inputStorageArrayJson);
+}
+
+function onFormSubmit(e) {
+  e.preventDefault();
+
+  formEl.reset();
+  console.log(formData);
+  localStorage.removeItem(keyLocaleStorage);
+}
+
+function textInAllFormInput() {
+  const savedMessage = localStorage.getItem(keyLocaleStorage);
+
+  if (savedMessage) {
+    formEl.email.value = keyLocaleStorage;
+    formEl.message.value = keyLocaleStorage;
+  }
+}
